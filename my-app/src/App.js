9e3ad315f4aff8b3ec244d741c3630175846d333
unknown <th.sahbi@gmail.com>
2021-08-18 01:19:36 +0300

    create projrct

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Axios  from './Component/Axios';
+function App() {
+  return (
+    <div className="App">
+     <Axios/>
+    </div>
+  )
+}
+export default App;
+
+
